de5cefd8a842ee2cdabe128e54d21cad53ad8344
hacubillos <hector.cubillos@fonterra.com>
2020-10-28 20:48:46 +1300

    Added a function that extracts a date from a string based on a list of regex patterns. This list is hardcoded and could be stored somewhere in a config for better referencing

--- src/date_extraction.py
+++ src/date_extraction.py
@@ -1,17 +1,34 @@
 import re
+import datetime as dt
 
 delimiter = "(\.|_| |-|\s)"
-#pattern_1 finds day-month-year with delimiters
-pattern_1 = "([1-9]|0[1-9]|1[\d]|2[\d]|3[0-1])"+delimiter+"([1-9]|0[1-9]|1[0-2])"+delimiter+"([\d][\d]|19[\d][\d]|20[\d][\d])"
-#pattern_2 finds year-month-day patterns with delimiters
-pattern_2 = "([\d][\d]|19[\d][\d]|20[\d][\d])"+delimiter+"([1-9]|0[1-9]|1[0-2])"+delimiter+"([1-9]|0[1-9]|1[\d]|2[\d]|3[0-1])"
-#pattern 3 finds strings of eight consecutive digits that match a year-month-day format
-pattern_3 = "(19[\d][\d]|20[\d][\d])(0[1-9]|1[0-2])(0[1-9]|1[\d]|2[\d]|3[0-1])"
-#pattern 4 finds strings of eight consecutive digits that match a day-month-year format
-pattern_4 = "(0[1-9]|1[\d]|2[\d]|3[0-1])(0[1-9]|1[0-2])(19[\d][\d]|20[\d][\d])"
-dates_regex_list  = [pattern_1, pattern_2, pattern_3, pattern_4]
+patterns_list = [{"pattern": "([1-9]|0[1-9]|1[\d]|2[\d]|3[0-1])"+delimiter+"([1-9]|0[1-9]|1[0-2])"+delimiter+"(19[\d][\d]|20[\d][\d])",
+                  "strp_format": "%d/%m/%Y"},
+                 {"pattern":"(19[\d][\d]|20[\d][\d])"+delimiter+"([1-9]|0[1-9]|1[0-2])"+delimiter+"([1-9]|0[1-9]|1[\d]|2[\d]|3[0-1])",
+                  "strp_format": "%Y/%m/%d"},
+                 {"pattern": "(19[\d][\d]|20[\d][\d])(0[1-9]|1[0-2])(0[1-9]|1[\d]|2[\d]|3[0-1])",
+                  "strp_format": "%Y%m%d"},
+                 {"pattern": "(0[1-9]|1[\d]|2[\d]|3[0-1])(0[1-9]|1[0-2])(19[\d][\d]|20[\d][\d])",
+                  "strp_format": "%d%m%Y"}
+                 ]
 
-for pattern in dates_regex_list:
-    match = re.search(pattern, 'letter_of_resignation_20201031.xlsx')
-    if match: break
+def extract_date_from_str(patterns_list, string):
+    """
+    This function extracts a date from a string using a list of regex patterns
+    :param patterns_list: list of pattern dictionaries [{pattern 1: regex, strp_format: datetime format}]
+    :param string: str
+    :return: datetime.date
+    """
+    for pt in patterns_list:
+        match = re.search(pt["pattern"], string)
+        if match:
+            match = match[0]
+            delim = re.search(delimiter, match)
+            if delim:
+                match = match.replace(delim[0], "/")
+            return dt.datetime.strptime(match, pt["strp_format"]).date()
+    return None
 
+if __name__ == '__main__':
+    string = 'letter_of_resignation_09012011.xlsx'
+    date = extract_date_from_str(patterns_list=patterns_list, string=string)
