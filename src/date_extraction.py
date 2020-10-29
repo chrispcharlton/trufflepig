74581d76a55720b9872f8d2b1103aa1ec3e71d4b
hacubillos <hector.cubillos@fonterra.com>
2020-10-30 10:09:12 +1300

    Added changes after code review

--- src/date_extraction.py
+++ src/date_extraction.py
@@ -1,34 +1,37 @@
 import re
-import datetime as dt
+from dateutil.parser import parse
 
-delimiter = "(\.|_| |-|\s)"
-patterns_list = [{"pattern": "([1-9]|0[1-9]|1[\d]|2[\d]|3[0-1])"+delimiter+"([1-9]|0[1-9]|1[0-2])"+delimiter+"(19[\d][\d]|20[\d][\d])",
-                  "strp_format": "%d/%m/%Y"},
-                 {"pattern":"(19[\d][\d]|20[\d][\d])"+delimiter+"([1-9]|0[1-9]|1[0-2])"+delimiter+"([1-9]|0[1-9]|1[\d]|2[\d]|3[0-1])",
-                  "strp_format": "%Y/%m/%d"},
-                 {"pattern": "(19[\d][\d]|20[\d][\d])(0[1-9]|1[0-2])(0[1-9]|1[\d]|2[\d]|3[0-1])",
-                  "strp_format": "%Y%m%d"},
-                 {"pattern": "(0[1-9]|1[\d]|2[\d]|3[0-1])(0[1-9]|1[0-2])(19[\d][\d]|20[\d][\d])",
-                  "strp_format": "%d%m%Y"}
+delimiter = "(\.| |-|\s||/)"
+patterns_list = [#14-digit delimited sequence in the following order (4-2-2 2:2:2.6)
+                 "\d{4}"+delimiter+"\d{2}"+delimiter+"\d{2} (\d\d:){2}\d{2}.\d{6}",
+                 ##14-digit delimited sequence in the following order (2-2-4 2:2:2.6)
+                 "\d{2}"+delimiter+"\d{2}"+delimiter+"\d{2} (\d\d:){2}\d{2}.\d{6}",
+                 #8-digit delimited (4-2-2) sequence
+                 "\d{4}"+delimiter+"\d{2}"+delimiter+"\d{2}",
+                 #8-digit delimited (4-2-2) sequence
+                 "\d{2}"+delimiter+"\d{2}"+delimiter+"\d{4}",
                  ]
 
 def extract_date_from_str(patterns_list, string):
     """
     This function extracts a date from a string using a list of regex patterns
-    :param patterns_list: list of pattern dictionaries [{pattern 1: regex, strp_format: datetime format}]
+    For the patterns extracted from string look at the patterns_list variable
+    For strings recognized as dates in the dateutil.parser module, take a look at https://stackabuse.com/converting-strings-to-datetime-in-python/
+    :param patterns_list: list of regex date patterns
     :param string: str
-    :return: datetime.date
+    :return: datetime.date or False
     """
     for pt in patterns_list:
-        match = re.search(pt["pattern"], string)
+        match = re.search(pt, string)
         if match:
             match = match[0]
-            delim = re.search(delimiter, match)
-            if delim:
-                match = match.replace(delim[0], "/")
-            return dt.datetime.strptime(match, pt["strp_format"]).date()
-    return None
+            try:
+                return parse(match)
+            except:
+                print(f"String {match} was not recognised as date. Check date patterns")
+                return False
+
 
 if __name__ == '__main__':
-    string = 'letter_of_resignation_09012011.xlsx'
+    string = 'letter_of_resignation_2011/01/01 08:15:27.123456.xlsx'
     date = extract_date_from_str(patterns_list=patterns_list, string=string)
