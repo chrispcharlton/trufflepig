38e1f024d38172216f538b0bf7fc666d2bbfa1a3
iansane <iansane.w@gmail.com>
2020-11-01 11:07:07 +1300

    adjust temporal plan example

--- examples/temporal_plans.py
+++ examples/temporal_plans.py
@@ -4,11 +4,10 @@ import src.main
 if __name__ == '__main__':
     # Task: find the latest plan in a directory
     # find all files in path
-    files=src.main.search(dir='./temporal_plans_dir')
     # sort by created time,
     # assuming latest created time equals the latest plan
-    plans=src.main.sort_paths(files,'ctime',descending=True)
-    latest_plan=plans[0]
-    print(latest_plan)
+    files=src.main.search(dir='./temporal_plans_dir',sort='ctime',descending=True)
+    print(files[0])
     #plan is not the latest plan
+
 
